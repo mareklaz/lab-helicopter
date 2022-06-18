43e7e7f6ee3ce2546d8b989549fb27585dacdfc6
mareklaz <mareklaz@MacBook-Air-de-Marek.local>
2022-06-18 12:08:53 +0200

    start btn and game.js added

--- game.js
+++ game.js
@@ -1,8 +1,13 @@
 class Game {
   constructor(ctx) {
+    this.ctx = ctx
+    this.helicopter = new Helicopter(this.ctx)
+    this.background = new Background(this.ctx)
   }
 
   start() {
+    this.draw()
+    this.move()
   }
 
   clearObstacles() {
--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const ctx = document.getElementById('canvas').getContext('2d');
 const game = new Game(ctx);
+
+window.onload = () => {
+    document.getElementById('startBtn').onclick = () => {
+        game.start();
+    };
+};
